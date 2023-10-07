9d77b8a70514ff504222926ddd01485818c40f01
Eduard Epurica <epurica.eduard@gmail.com>
2023-10-07 17:39:07 +0300

    Added Actuator Dependency

--- src/main/java/com/eduard/springboot/demo/elearning/rest/FirstRestController.java
+++ src/main/java/com/eduard/springboot/demo/elearning/rest/FirstRestController.java
@@ -9,9 +9,23 @@ public class FirstRestController {
 
     }
 
-    @GetMapping
+    @GetMapping("/")
     public String sayHello () {
         return "Hello World";
     }
+
+    //Expose a new REST endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    //Expose a new REST endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Bad luck today";
+    }
+
+
 
 }
